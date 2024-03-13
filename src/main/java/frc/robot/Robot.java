d3828c9d495fbaa4fcda303caa73090c4e94f817
GatorBytes8303 <158369989+GatorBytes8303@users.noreply.github.com>
2024-03-13 13:30:26 -0700

    Camera updated code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,19 +20,24 @@ import edu.wpi.first.cameraserver.CameraServer;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-
-
   private RobotContainer m_robotContainer;
 
+  private UsbCamera frontCamera;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
+    // Starts camera capture and sets config
+    frontCamera = CameraServer.startAutomaticCapture(0);
+    frontCamera.setResolution(320, 240);
+    frontCamera.setExposureAuto();
+    frontCamera.setFPS(15);
+    frontCamera.setBrightness(43);
+    
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-      CameraServer.startAutomaticCapture();
       m_robotContainer = new RobotContainer();
   }
 
